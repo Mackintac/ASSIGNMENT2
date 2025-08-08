4c44e3fcc3909bca508434d2b6b4236f7b20ff34
Mackintac <mack-robinson@hotmail.ca>
2025-08-08 18:29:43 -0400

    add routes to future pages

--- ASSIGNMENT2/routes/index.js
+++ ASSIGNMENT2/routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Independent Study Tracker' });
 });
 
+router.get('/login', function (req, res, next) {
+  res.render('login', { title: 'Login' });
+});
+
+router.get('/register', function (req, res, next) {
+  res.render('register', { title: 'Register' });
+});
+
+router.get('/about', function (req, res, next) {
+  res.render('about', { title: 'About this app' });
+});
+
 module.exports = router;
